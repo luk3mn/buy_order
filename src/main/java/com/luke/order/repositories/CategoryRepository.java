ee082d3bcf1f91792756eff5a673381f277da945
luk3mn <lucasrenan2015@gmail.com>
2023-11-08 09:33:28 -0300

    Category

---
+++ src/main/java/com/luke/order/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.luke.order.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luke.order.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+}
