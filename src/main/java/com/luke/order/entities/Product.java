2444b3d2d207bb17a0cc8d131f904b8ee393fc38
luk3mn <lucasrenan2015@gmail.com>
2023-12-14 16:17:23 -0300

    feat: Product-OrderItem one-to-many association

--- src/main/java/com/luke/order/entities/Product.java
+++ src/main/java/com/luke/order/entities/Product.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,9 @@ public class Product implements Serializable {
     @ManyToMany @Fetch(FetchMode.JOIN)
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product") 
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -86,6 +92,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
