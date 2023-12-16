9a5e4e60c7adf9eb3248b7dc10234c6123c5ecbd
luk3mn <lucasrenan2015@gmail.com>
2023-12-16 16:50:40 -0300

    feat: Exception handling - findById

--- src/main/java/com/luke/order/services/UserService.java
+++ src/main/java/com/luke/order/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.luke.order.entities.User;
 import com.luke.order.repositories.UserRepository;
+import com.luke.order.services.exceptions.ResourceNotFoundException;
 
 @Service // register this class as a component, to be able to inject automatically with Autowired
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
